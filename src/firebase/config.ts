dae5d0bc2ad0acbdfe762b30aeaa5c6a2139c49b
Pred Finance <predfi.xyz@gmail.com>
2025-09-25 07:33:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5210962417-9bc8d",
+  "appId": "1:295616279465:web:652a2ba4006aa1989ccab8",
+  "apiKey": "AIzaSyCDzQS_DuvybQDMMbOXQaXRUae9eNGHUoM",
+  "authDomain": "studio-5210962417-9bc8d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "295616279465"
+};

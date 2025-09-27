3a13cebf0e90d37588a55e5a4d9ff0d6afbe8031
Pred Finance <predfi.xyz@gmail.com>
2025-09-27 06:37:27 +0000

    bro the config put it in the .env.local  apiKey: "AIzaSyCDzQS_DuvybQDMMb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyCDzQS_DuvybQDMMbOXQaXRUae9eNGHUoM",
-  authDomain: "studio-5210962417-9bc8d.firebaseapp.com",
-  projectId: "studio-5210962417-9bc8d",
-  storageBucket: "studio-5210962417-9bc8d.firebasestorage.app",
-  messagingSenderId: "295616279465",
-  appId: "1:295616279465:web:652a2ba4006aa1989ccab8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
